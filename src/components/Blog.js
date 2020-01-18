231a2f7a4727c2b90cbea8ccface0a9235067464
Diego Arturo MG <diegoarturo2121@gmail.com>
2020-01-18 00:50:15 -0600

    Blog

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -1,9 +1,26 @@
 import React, { Component } from 'react';
 import Slider from './Slider';
 import Sidebar from './Sidebar';
+import axios from 'axios';
 
 class Blog extends Component {
+
+    state = {
+        articles: {},
+        status: null
+    }
+
     render() {
+
+        axios.get("http://localhost:3900/api/articles")
+            .then(res => {
+                console.log(res.data);
+                this.setState({
+                    articles: res.data.articles,
+                    status: 'success'
+                });
+            });
+
         return (
             <div id="blog">
                 <Slider
@@ -12,11 +29,19 @@ class Blog extends Component {
                 />
                 <div className="center">
                     <div id="content">
-                        {
-                            /*Listado de articulos*/
+                        {/*Listado de articulos*/}
+
+                        {this.state.status === 'success' &&
+                            <div>
+                                {
+                                    this.state.articles.map((article) => {
+                                        return (<h1 key={article._id}>{article.title}</h1>)
+                                    })
+                                }
+                            </div>
                         }
                     </div>
-                    <Sidebar 
+                    <Sidebar
                         blog="true"
                     />
                 </div>
